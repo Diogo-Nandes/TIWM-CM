beb7beb56df07ba55148c9d797cb6d1f61ad057b
Diogo-Nandes <120397281+Diogo-Nandes@users.noreply.github.com>
2025-01-12 19:34:01 +0000

    Update MainActivity.kt

---
+++ ImcApp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.imcapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.imcapp.ui.theme.IMCAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var isDarkTheme by remember { mutableStateOf(false) }
+            IMCAppTheme(darkTheme = isDarkTheme) {
+                MyApp(
+                    isDarkTheme = isDarkTheme,
+                    onThemeToggle = { isDarkTheme = !isDarkTheme }
+                )
+            }
+        }
+    }
+}
